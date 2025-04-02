15d6713630dd40f3f5ecb7e43cc94eafd4cd570a
Roy Buhay <roydesign@outlook.com>
2025-04-02 23:08:46 +0800

    chore: v2 init

---
+++ src/data/about.ts
@@ -0,0 +1,61 @@
+export const about = [
+  {
+    timestamp: "Present",
+    company: "Avazoo Holdings",
+    name: "avazoo",
+    icon: "avazoo-logo.png",
+    role: "UI Designer & Full-stack Developer",
+    description: [
+      'Led complete mobile app overhaul using React Native & Expo, increasing user engagement, load times and introducing new features like seamless referral system, merchant payment integration and more',
+      'Integrated InertiaJS with Laravel 8 to handle large datasets more efficiently, improving back-office tool performance bydreducing data processing time from ~2 minutes to under 1 minute',
+      'Set up automated CI/CD pipelines using GitLab CI/CD and automated testing with Jest before AWS deployments, streamlining releases and minimizing downtime'
+    ],
+  },
+  {
+    timestamp: "2018 - 2021",
+    company: "Apptech Gateway",
+    name: "apptech",
+    icon: "apptech-logo.png",
+    role: "UI Designer & Full-stack Developer",
+    description: [
+      'Developed a scalable match-it game using ReactJS & Redux with complex state management across multiple product integrations',
+      'Developed RESTful API endpoints in Node.js/Express handling game logic and supporting concurrent user interactions',
+      'Optimized Laravel dashboard using performance tools, resulting in 3x faster data rendering and improved user satisfaction'
+    ],
+  },
+  {
+    timestamp: "2018 - 2021",
+    company: "Entinux Ltd.",
+    name: "entinux",
+    icon: "entinux-logo.png",
+    role: "UI Designer & Front-end Developer",
+    description: [
+      'Built a reward system mobile app with Ionic/Angular, handling high concurrent user actions daily from various activities in Hong Kong',
+      'Implemented WCAG accessibility standards expanding product reach and ensuring compliance with web accessibility guidelines',
+      'Led UI/UX design using Figma for a university feedback system, crowdsourcing app, and email management tool and more using user-centered design principles to enhance user experience'
+    ],
+  },
+  {
+    timestamp: "2021 - 2022",
+    company: "Crateclub",
+    name: "crateclub",
+    icon: "crateclub-logo.png",
+    role: "UI Designer & Front-end Developer",
+    description: [
+      'Spearheaded front-end development workflows by introducing modern tooling like Sass, ESLint, and Sentry and setting coding standards',
+      'Implemented front-end features from Figma designs and integrated payment solutions using Stripe and PayPal',
+      'Designed UI/UX using Figma for mobile and web applications (back-office dashboards)'
+    ],
+  },
+  {
+    timestamp: "2015 - 2018",
+    company: "Nexusbond Asia Inc.",
+    name: "nexusbond",
+    icon: "nexusbond-logo.png",
+    role: "Senior Developer / Designer",
+    description: [
+      'Developed, maintained, and shipped production code for client websites primarily using HTML, CSS, Sass, JavaScript, and jQuery',
+      'Performed quality assurance tests on various sites to ensure cross browser compatibility and mobile responsiveness'
+    ],
+  },
+]
---
+++ src/data/stack.ts
@@ -0,0 +1,14 @@
+export const stacks = [
+    'css',
+    'html',
+    'js',
+    'ts',
+    'react',
+    'react_native',
+    'tailwind',
+    'nextjs',
+    'graphql',
+    'aws',
+    'node',
+    'express'
+]
